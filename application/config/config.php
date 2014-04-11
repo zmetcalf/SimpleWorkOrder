86e5406f09cebac1eeb99d3ef40145ac9639dfab
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-04-11 17:47:31 -0600

    Changed session settings

--- application/config/config.php
+++ application/config/config.php
@@ -245,14 +245,14 @@ $config['encryption_key'] = 'you_must_set_encryption_key';
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 32400; // 9 hours
+$config['sess_expiration']		= 604800; // One week
 $config['sess_expire_on_close']	= TRUE;
 $config['sess_encrypt_cookie']	= TRUE;
-$config['sess_use_database']	= FALSE;
+$config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= TRUE;
 $config['sess_match_useragent']	= TRUE;
-$config['sess_time_to_update']	= 300;
+$config['sess_time_to_update']	= 604800;
 
 /*
 |--------------------------------------------------------------------------
