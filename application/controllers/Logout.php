c4de119f162508176b5a92c383d4a7102f3eb5a6
Zachery Metcalf <zachery.metcalf@gmail.com>
2015-04-02 20:41:58 -0600

    Start to debug codigniter - need to remove controller calls"

--- application/controllers/Logout.php
+++ application/controllers/Logout.php
@@ -5,7 +5,6 @@ class Logout extends CI_Controller {
   public function index()
   {
     $this->session->sess_destroy();
-    $this->load->library('../controllers/login');
-    $this->login->index();
+    redirect('/login/');
   }
 }
